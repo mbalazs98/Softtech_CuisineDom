d00033f3233b3680706e6d3876021abd4f3ebba1
szilviaszeier <szilvia.szeier@gmail.com>
2020-10-22 18:02:52 +0200

    database specifications

--- backend/api/db.sql
+++ backend/api/db.sql
@@ -2,27 +2,23 @@ BEGIN;
 --
 -- Create model cuisines
 --
-CREATE TABLE "recipes_cuisines" ("cuisines_id" integer NOT NULL PRIMARY KEY, "cuisines_name" text NOT NULL UNIQUE);
+CREATE TABLE "recipes_cuisines" ("cuisines_id" integer NOT NULL PRIMARY KEY AUTOINCREMENT, "cuisines_name" varchar(50) NOT NULL UNIQUE);
 --
 -- Create model ingredients
 --
-CREATE TABLE "recipes_ingredients" ("ingredient_id" integer NOT NULL PRIMARY KEY, "ingredient_name" text NOT NULL UNIQUE);
+CREATE TABLE "recipes_ingredients" ("ingredient_id" integer NOT NULL PRIMARY KEY AUTOINCREMENT, "ingredient_name" varchar(50) NOT NULL UNIQUE);
 --
 -- Create model recipes
 --
-CREATE TABLE "recipes_recipes" ("recipe_id" integer NOT NULL PRIMARY KEY, "recipe_name" text NOT NULL UNIQUE, "cooking_method" text NOT NULL UNIQUE, "image" text NULL, "string_ingredients" text NOT NULL, "prep_time" integer NULL, "serving" integer NULL);
+CREATE TABLE "recipes_recipes" ("recipe_id" integer NOT NULL PRIMARY KEY AUTOINCREMENT, "recipe_name" varchar(50) NOT NULL, "cooking_method" text NOT NULL UNIQUE, "image" text NULL, "string_ingredients" text NOT NULL, "prep_time" integer NULL, "serving" integer NULL);
 --
 -- Create model tags
 --
-CREATE TABLE "recipes_tags" ("tag_id" integer NOT NULL PRIMARY KEY, "tag_name" text NOT NULL UNIQUE);
+CREATE TABLE "recipes_tags" ("tag_id" integer NOT NULL PRIMARY KEY AUTOINCREMENT, "tag_name" varchar(50) NOT NULL UNIQUE);
 --
 -- Create model users
 --
-CREATE TABLE "recipes_users" ("user_id" integer NOT NULL PRIMARY KEY, "username" text NOT NULL UNIQUE, "password" text NOT NULL, "email" text NOT NULL UNIQUE);
---
--- Create model recipe_cuisines
---
-CREATE TABLE "recipes_recipe_cuisines" ("id" integer NOT NULL PRIMARY KEY AUTOINCREMENT, "cuisines_id_id" integer NOT NULL REFERENCES "recipes_cuisines" ("cuisines_id") DEFERRABLE INITIALLY DEFERRED, "recipe_id_id" integer NOT NULL REFERENCES "recipes_recipes" ("recipe_id") DEFERRABLE INITIALLY DEFERRED);
+CREATE TABLE "recipes_users" ("user_id" integer NOT NULL PRIMARY KEY AUTOINCREMENT, "username" varchar(50) NOT NULL UNIQUE, "password" varchar(50) NOT NULL, "email" varchar(100) NOT NULL UNIQUE);
 --
 -- Create model user_recipes
 --
@@ -35,15 +31,50 @@ CREATE TABLE "recipes_recipe_tags" ("id" integer NOT NULL PRIMARY KEY AUTOINCREM
 -- Create model recipe_ingredients
 --
 CREATE TABLE "recipes_recipe_ingredients" ("id" integer NOT NULL PRIMARY KEY AUTOINCREMENT, "ingredient_id_id" integer NOT NULL REFERENCES "recipes_ingredients" ("ingredient_id") DEFERRABLE INITIALLY DEFERRED, "recipe_id_id" integer NOT NULL REFERENCES "recipes_recipes" ("recipe_id") DEFERRABLE INITIALLY DEFERRED);
-CREATE INDEX "recipes_recipe_cuisines_cuisines_id_id_eff2ccfc" ON "recipes_recipe_cuisines" ("cuisines_id_id");
-CREATE INDEX "recipes_recipe_cuisines_recipe_id_id_bb014f15" ON "recipes_recipe_cuisines" ("recipe_id_id");
-CREATE UNIQUE INDEX "recipes_user_recipes_recipe_id_id_user_id_id_bf8362e5_uniq" ON "recipes_user_recipes" ("recipe_id_id", "user_id_id");
-CREATE INDEX "recipes_user_recipes_recipe_id_id_a798f2c8" ON "recipes_user_recipes" ("recipe_id_id");
-CREATE INDEX "recipes_user_recipes_user_id_id_7cbb16b8" ON "recipes_user_recipes" ("user_id_id");
-CREATE UNIQUE INDEX "recipes_recipe_tags_recipe_id_id_tag_id_id_543e754a_uniq" ON "recipes_recipe_tags" ("recipe_id_id", "tag_id_id");
+--
+-- Create model recipe_cuisines
+--
+CREATE TABLE "recipes_recipe_cuisines" ("id" integer NOT NULL PRIMARY KEY AUTOINCREMENT, "cuisines_id_id" integer NOT NULL REFERENCES "recipes_cuisines" ("cuisines_id") DEFERRABLE INITIALLY DEFERRED, "recipe_id_id" integer NOT NULL REFERENCES "recipes_recipes" ("recipe_id") DEFERRABLE INITIALLY DEFERRED);
+--
+-- Create constraint user_recipes_id on model user_recipes
+--
+CREATE TABLE "new__recipes_user_recipes" ("id" integer NOT NULL PRIMARY KEY AUTOINCREMENT, "recipe_id_id" integer NOT NULL REFERENCES "recipes_recipes" ("recipe_id") DEFERRABLE INITIALLY DEFERRED, "user_id_id" integer NOT NULL REFERENCES "recipes_users" ("user_id") DEFERRABLE INITIALLY DEFERRED, CONSTRAINT "user_recipes_id" UNIQUE ("recipe_id_id", "user_id_id"));
+INSERT INTO "new__recipes_user_recipes" ("id", "recipe_id_id", "user_id_id") SELECT "id", "recipe_id_id", "user_id_id" FROM "recipes_user_recipes";
+DROP TABLE "recipes_user_recipes";
+ALTER TABLE "new__recipes_user_recipes" RENAME TO "recipes_user_recipes";
 CREATE INDEX "recipes_recipe_tags_recipe_id_id_7b51e8e2" ON "recipes_recipe_tags" ("recipe_id_id");
 CREATE INDEX "recipes_recipe_tags_tag_id_id_65797fb2" ON "recipes_recipe_tags" ("tag_id_id");
-CREATE UNIQUE INDEX "recipes_recipe_ingredients_recipe_id_id_ingredient_id_id_e36a30bc_uniq" ON "recipes_recipe_ingredients" ("recipe_id_id", "ingredient_id_id");
 CREATE INDEX "recipes_recipe_ingredients_ingredient_id_id_bd863c17" ON "recipes_recipe_ingredients" ("ingredient_id_id");
 CREATE INDEX "recipes_recipe_ingredients_recipe_id_id_2af7aac7" ON "recipes_recipe_ingredients" ("recipe_id_id");
+CREATE INDEX "recipes_recipe_cuisines_cuisines_id_id_eff2ccfc" ON "recipes_recipe_cuisines" ("cuisines_id_id");
+CREATE INDEX "recipes_recipe_cuisines_recipe_id_id_bb014f15" ON "recipes_recipe_cuisines" ("recipe_id_id");
+CREATE INDEX "recipes_user_recipes_recipe_id_id_a798f2c8" ON "recipes_user_recipes" ("recipe_id_id");
+CREATE INDEX "recipes_user_recipes_user_id_id_7cbb16b8" ON "recipes_user_recipes" ("user_id_id");
+--
+-- Create constraint recipe_tags_id on model recipe_tags
+--
+CREATE TABLE "new__recipes_recipe_tags" ("id" integer NOT NULL PRIMARY KEY AUTOINCREMENT, "recipe_id_id" integer NOT NULL REFERENCES "recipes_recipes" ("recipe_id") DEFERRABLE INITIALLY DEFERRED, "tag_id_id" integer NOT NULL REFERENCES "recipes_tags" ("tag_id") DEFERRABLE INITIALLY DEFERRED, CONSTRAINT "recipe_tags_id" UNIQUE ("recipe_id_id", "tag_id_id"));
+INSERT INTO "new__recipes_recipe_tags" ("id", "recipe_id_id", "tag_id_id") SELECT "id", "recipe_id_id", "tag_id_id" FROM "recipes_recipe_tags";
+DROP TABLE "recipes_recipe_tags";
+ALTER TABLE "new__recipes_recipe_tags" RENAME TO "recipes_recipe_tags";
+CREATE INDEX "recipes_recipe_tags_recipe_id_id_7b51e8e2" ON "recipes_recipe_tags" ("recipe_id_id");
+CREATE INDEX "recipes_recipe_tags_tag_id_id_65797fb2" ON "recipes_recipe_tags" ("tag_id_id");
+--
+-- Create constraint recipe_ingredients_id on model recipe_ingredients
+--
+CREATE TABLE "new__recipes_recipe_ingredients" ("id" integer NOT NULL PRIMARY KEY AUTOINCREMENT, "ingredient_id_id" integer NOT NULL REFERENCES "recipes_ingredients" ("ingredient_id") DEFERRABLE INITIALLY DEFERRED, "recipe_id_id" integer NOT NULL REFERENCES "recipes_recipes" ("recipe_id") DEFERRABLE INITIALLY DEFERRED, CONSTRAINT "recipe_ingredients_id" UNIQUE ("recipe_id_id", "ingredient_id_id"));
+INSERT INTO "new__recipes_recipe_ingredients" ("id", "ingredient_id_id", "recipe_id_id") SELECT "id", "ingredient_id_id", "recipe_id_id" FROM "recipes_recipe_ingredients";
+DROP TABLE "recipes_recipe_ingredients";
+ALTER TABLE "new__recipes_recipe_ingredients" RENAME TO "recipes_recipe_ingredients";
+CREATE INDEX "recipes_recipe_ingredients_ingredient_id_id_bd863c17" ON "recipes_recipe_ingredients" ("ingredient_id_id");
+CREATE INDEX "recipes_recipe_ingredients_recipe_id_id_2af7aac7" ON "recipes_recipe_ingredients" ("recipe_id_id");
+--
+-- Create constraint recipe_cuisines_id on model recipe_cuisines
+--
+CREATE TABLE "new__recipes_recipe_cuisines" ("id" integer NOT NULL PRIMARY KEY AUTOINCREMENT, "cuisines_id_id" integer NOT NULL REFERENCES "recipes_cuisines" ("cuisines_id") DEFERRABLE INITIALLY DEFERRED, "recipe_id_id" integer NOT NULL REFERENCES "recipes_recipes" ("recipe_id") DEFERRABLE INITIALLY DEFERRED, CONSTRAINT "recipe_cuisines_id" UNIQUE ("recipe_id_id", "cuisines_id_id"));
+INSERT INTO "new__recipes_recipe_cuisines" ("id", "cuisines_id_id", "recipe_id_id") SELECT "id", "cuisines_id_id", "recipe_id_id" FROM "recipes_recipe_cuisines";
+DROP TABLE "recipes_recipe_cuisines";
+ALTER TABLE "new__recipes_recipe_cuisines" RENAME TO "recipes_recipe_cuisines";
+CREATE INDEX "recipes_recipe_cuisines_cuisines_id_id_eff2ccfc" ON "recipes_recipe_cuisines" ("cuisines_id_id");
+CREATE INDEX "recipes_recipe_cuisines_recipe_id_id_bb014f15" ON "recipes_recipe_cuisines" ("recipe_id_id");
 COMMIT;
--- backend/api/recipes/models.py
+++ backend/api/recipes/models.py
@@ -2,27 +2,27 @@ from django.db import models
 
 
 class tags(models.Model):
-    tag_id = models.IntegerField(primary_key=True)
-    tag_name = models.TextField(unique=True) 
+    tag_id = models.AutoField(primary_key=True)
+    tag_name = models.CharField(max_length=50, unique=True) 
 
 
 class ingredients(models.Model):
-    ingredient_id = models.IntegerField(primary_key=True)
-    ingredient_name = models.TextField(unique=True) 
+    ingredient_id = models.AutoField(primary_key=True)
+    ingredient_name = models.CharField(max_length=50, unique=True) 
     
 class cuisines(models.Model):
-    cuisines_id = models.IntegerField(primary_key=True)
-    cuisines_name = models.TextField(unique=True) 
+    cuisines_id = models.AutoField(primary_key=True)
+    cuisines_name = models.CharField(max_length=50, unique=True) 
     
 class users(models.Model):
-    user_id = models.IntegerField(primary_key=True)
-    username = models.TextField(unique=True)
-    password = models.TextField()
-    email = models.TextField(unique=True) 
+    user_id = models.AutoField(primary_key=True)
+    username = models.CharField(max_length=50, unique=True)
+    password = models.CharField(max_length=50)
+    email = models.CharField(max_length=100, unique=True) 
     
 class recipes(models.Model):
-    recipe_id = models.IntegerField(primary_key=True)
-    recipe_name = models.TextField(unique=True)
+    recipe_id = models.AutoField(primary_key=True)
+    recipe_name = models.CharField(max_length=50)
     cooking_method = models.TextField(unique=True)
     image = models.TextField(null=True)
     string_ingredients = models.TextField()
@@ -32,23 +32,33 @@ class recipes(models.Model):
 class recipe_cuisines(models.Model):
     recipe_id = models.ForeignKey(recipes, on_delete=models.CASCADE)
     cuisines_id = models.ForeignKey(cuisines, on_delete=models.CASCADE)
+    class Meta:
+        constraints = [
+        models.UniqueConstraint(fields= ["recipe_id", "cuisines_id"], name='recipe_cuisines_id'),
+        ]
     
 class recipe_tags(models.Model):
     recipe_id = models.ForeignKey(recipes, on_delete=models.CASCADE)
     tag_id = models.ForeignKey(tags, on_delete=models.CASCADE)
     class Meta:
-        unique_together = (("recipe_id", "tag_id"),)
+        constraints = [
+        models.UniqueConstraint(fields= ["recipe_id", "tag_id"], name='recipe_tags_id'),
+        ]
     
     
 class recipe_ingredients(models.Model):
     recipe_id = models.ForeignKey(recipes, on_delete=models.CASCADE)
     ingredient_id = models.ForeignKey(ingredients, on_delete=models.CASCADE)
     class Meta:
-        unique_together = (("recipe_id", "ingredient_id"),)
+        constraints = [
+        models.UniqueConstraint(fields= ["recipe_id", "ingredient_id"], name='recipe_ingredients_id'),
+        ]
     
 class user_recipes(models.Model):
     recipe_id = models.ForeignKey(recipes, on_delete=models.CASCADE)
     user_id = models.ForeignKey(users, on_delete=models.CASCADE)
     class Meta:
-        unique_together = (("recipe_id", "user_id"),)
+        constraints = [
+        models.UniqueConstraint(fields= ["recipe_id", "user_id"], name='user_recipes_id'),
+        ]
     
