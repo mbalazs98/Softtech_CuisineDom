bfe4b6086cec590cd4dc04318ab956cd50a1ba44
mbalazs98 <49280623+mbalazs98@users.noreply.github.com>
2020-11-05 19:41:10 +0100

    Update tests.py

--- backend/api/recipes/tests.py
+++ backend/api/recipes/tests.py
@@ -31,4 +31,30 @@ class LoginTestCase(TestCase):
 
         self.assertEqual(test_user2.username, 'test_user')
         self.assertEqual(test_user2.email, 'test@email.com')
+    def test_user_authenticate(self):
+        user = User.objects.create_user(username='test_user_neg', password='test_password_neg',email='test@email.com_neg')
+        user.save()
+
+
+        recipes_user = users.objects.create()
+        recipes_user.username = 'test_user'
+        recipes_user.password = 'test_password'
+        recipes_user.email = 'test@email.com'
+
+        #recipes_user = users.objects.create_user(username='test_user', password='test_password', email='test@email.com')
+
+        recipes_user.save()
+
+        test_user1 = users.objects.get(username='test_user')
+
+        self.assertNotEqual(test_user1.username, 'test_user_neg')
+        self.assertNotEqual(test_user1.password, 'test_password_neg')
+        self.assertNotEqual(test_user1.email, 'test@email.com_neg')
+
+        test_user2 = authenticate(username='test_user_neg', password='test_password_neg')
+
+        self.assertIsNotNone(test_user2)
+
+        self.assertNotEqual(test_user2.username, 'test_user')
+        self.assertNotEqual(test_user2.email, 'test@email.com')
 
