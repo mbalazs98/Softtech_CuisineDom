c93fe9f624e25e135506f39d76a2af997d9392fd
mbalazs98 <49280623+mbalazs98@users.noreply.github.com>
2020-11-19 09:44:47 +0100

    Update settings.py

--- backend/api/api/settings.py
+++ backend/api/api/settings.py
@@ -64,6 +64,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django_pycdi.middlewares.CDIMiddleware',
 ]
 
 ROOT_URLCONF = 'api.urls'
